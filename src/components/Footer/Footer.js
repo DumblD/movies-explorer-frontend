9e09ba6687e8d6cf5650b52080fcb035bdd5fa8b
Artyom Zinin <NicYKN@yandex.ru>
2023-08-15 03:38:45 +0300

    fix: section with className footer -> footer with className footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,15 +3,14 @@ import React from 'react';
 function Footer() {
 
   return (
-    <section className='footer'>
+    <footer className='footer'>
       <p className="footer__study-project-text">Учебный проект Яндекс.Практикум х BeatFilm.</p>
       <ul className='footer__list-items'>
         <li className="footer__copyright">&copy; 2023. Артем Зинин</li>
         <li className="footer__yandex-practicum">Яндекс.Практикум</li>
         <li className="footer__github">Github</li>
       </ul>
-
-    </section>
+    </footer>
   );
 }
 
