69b939ded94753fdae5df227b71261e76f44dbe4
unknown <nicykn@yandex.ru>
2025-11-11 10:02:58 +0300

    info about updated

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -9,7 +9,7 @@ function AboutMe() {
       <ProjectSectionHeading sectionClassName={"about-me"} headingTitle={"Студент"} />
       <div className="about-me__content">
         <div className="about-me__info">
-          <h3 className="about-me__name">Артем</h3>
+          <h3 className="about-me__name">Артём</h3>
           <h4 className="about-me__major-in">Веб-разработчик</h4>
           <p className="about-me__description">Кодить нравится. Ищу стажировку / работу в сфере веб-разработки.</p>
           <a href="https://github.com/DumblD" className="about-me__github-link" target="_blank" rel="noreferrer">Github</a>
